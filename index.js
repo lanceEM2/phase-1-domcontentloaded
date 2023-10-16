e96236045c17e48c73299a71774c671d2d99f31d
Dylan <dylanlancekemboi@gmail.com>
2023-10-16 11:31:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    console.log("The DOM has loaded");
+    const text = document.getElementById('text');
+    text.textContent = "This is really cool!";
+    console.log(text);
+});
